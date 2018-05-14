b15f02c70efb58de5f1402f52da1b082fb6f7bde
Anshuman Verma <ansumanv@gmail.com>
2018-05-14 11:00:56 +0530

    Add Auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
 socket.on('connect', function () {
     console.log('Connected to server');
     socket.emit('createEmail', {
@@ -22,6 +37,7 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -33,6 +49,7 @@ socket.on('newLocationMessage', function (message) {
         url: message.url
     });
     jQuery('#messages').append(html);  
+    scrollToBottom();
 
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current Location</a>');
